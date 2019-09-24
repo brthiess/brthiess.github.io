47afae113d2f58c38e6bf62b83df79c20ab24dd3
Brad Thiessen <bthiessen@lawdepot.com>
2019-09-24 13:38:27 -0600

    blah

--- Calendar/wwwroot/js/site.js
+++ Calendar/wwwroot/js/site.js
@@ -21,17 +21,60 @@ function getQuestions(createNewQuiz) {
 
 function populateQuizHtml(data) {
 	var $quiz = $("[data-quiz-template]").clone();
-	var quizHtml = buildQuizHtml($quiz, data);
-	$("[data-quiz-container]").html(quizHtml);
+	var quizHtml = populateQuizTemplates($quiz, data);
+	$(quizHtml).appendTo($("[data-quiz-container]"));
 }
 
-function buildQuizHtml(template, data) {
+function populateQuizTemplates(template, data) {
+	var quizHtml = $(template).clone();
+	for (var i = 0; i < data.questions.length; i++) {
+		var quizSection = populateQuizSection($(template).find("[data-quiz-section]"), data.questions[i]);
+		$(quizSection).appendTo($(quizHtml));
+	}
+	return quizHtml;
+}
 
+function populateQuizSection(sectionTemplate, questionData) {
+	var quizSection = sectionTemplate.clone();
+	$(quizSection).find("[data-quiz-question]").text(questionData.question);
+	return quizSection;
 }
 
 function getFakeData() {
 	var fakeData = {
 		questions: [
+			{
+				type: "normal",
+				answers: [
+					{
+						text: "answer 1"
+					},
+					{
+						text: "answer 2"
+					},
+					{
+						text: "answer 3",
+						correct: true
+					}
+				],
+				question: "Pick the fiction"
+			},
+			{
+				type: "normal",
+				answers: [
+					{
+						text: "answer 2234"
+					},
+					{
+						text: "234234234 2"
+					},
+					{
+						text: "answer 3",
+						correct: true
+					}
+				],
+				question: "Pick the fiction"
+			},
 			{
 				type: "normal",
 				answers: [
