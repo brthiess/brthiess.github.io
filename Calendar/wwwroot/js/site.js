3c5ca00cfddf18aab8045518cd1f8c3b7e555291
Brad Thiessen <bthiessen@lawdepot.com>
2019-10-02 20:53:16 -0600

    ASDF

--- Calendar/wwwroot/js/site.js
+++ Calendar/wwwroot/js/site.js
@@ -2,7 +2,6 @@
 function beginQuiz() {
 	setupQuizUI();
 	getQuestions(true).then(function (data) {
-		console.log(data);
 		populateQuizHtml(data);
 		totalNumberOfQuestions = data.questions.length;
 		goToQuestionNumber(data.activeQuestionNumber);
@@ -24,7 +23,7 @@ function getQuestions(createNewQuiz) {
 
 function populateQuizHtml(data) {
 	var $quiz = $("[data-quiz-template]").clone();
-	var quizHtml = populateQuizTemplates($quiz, data);
+	var quizHtml = populateQuizTemplates($($quiz).find("[data-quiz-section-template]"), data);
 	$(quizHtml).appendTo($("[data-quiz-container]"));
 }
 
@@ -88,8 +87,31 @@ function populateQuizQuestion(questionTemplate, questionText) {
 
 function populateAnswer(answerTemplate, answer) {
 	var answerHtml = answerTemplate.clone();
-	$(answerHtml).text(answer.text);
+
+	var rogueAnswers = getRogueAnswersHtml($(answerTemplate).find("[data-quiz-rogue-answers]"), answer.rogues);
+	$(answerHtml).find("[data-quiz-rogue-answers]").replaceWith(rogueAnswers);
+
+	$(answerHtml).find("[data-quiz-answer-text]").text(answer.text);
+	$(answerHtml).attr("data-answer-number", answer.number);
+	$(answerHtml).attr("data-answer-correct", answer.correct);
 	return answerHtml;
+}
+
+function getRogueAnswersHtml(rogueAnswersTemplate, rogueAnswers) {
+	var rogueAnswersHtml = rogueAnswersTemplate.clone().html("");
+	if (typeof (rogueAnswers) !== 'undefined') {
+		for (var i = 0; i < rogueAnswers.length; i++) {
+			var rogueAnswer = populateRogueAnswer($(rogueAnswersTemplate).find("[data-quiz-rogue-answer]"), rogueAnswers[i]);
+			$(rogueAnswer).appendTo($(rogueAnswersHtml));
+		}
+	}
+	return rogueAnswersHtml;
+}
+
+function populateRogueAnswer(rogueAnswerTemplate, rogueAnswer) {
+	var rogueAnswerHtml = rogueAnswerTemplate.clone();
+	$(rogueAnswerHtml).find("[data-quiz-rogue-image]").attr("src", "/images/" + rogueAnswer.image);
+	return rogueAnswerHtml;
 }
 
 function goToNextQuestion() {
@@ -98,7 +120,7 @@ function goToNextQuestion() {
 }
 
 function goToPreviousQuestion() {
-	previousQuestionNumber = getCurrentQuestionNumber() + -1
+	previousQuestionNumber = getCurrentQuestionNumber() + -1;
 	goToQuestionNumber(previousQuestionNumber);
 }
 
@@ -161,45 +183,67 @@ function questionNumberExists(questionNumber) {
 	return $("[data-quiz-question-number=" + questionNumber + "]").length > 0;
 }
 
+$("body").on("click", "[data-quiz-answer]", function () {
+	var quizSection = $(this).closest("[data-quiz-section]");
+	if ($(quizSection).hasClass("answered")) {
+		return;
+	}
+	$(quizSection).find("[data-change-when-answered]").addClass("answered");
+	$(quizSection).addClass("answered");
+	if ($(this).attr("data-answer-correct") === "false") {
+		$(this).addClass("incorrect");
+	}
+});
+
 function getFakeData() {
 	var fakeData = {
-		activeQuestionNumber: 2,
+		activeQuestionNumber: 1,
 		questions: [
 			{
 				type: "normal",
 				answers: [
 					{
-						text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet tortor eu justo sodales vehicula. Nulla auctor tempus arcu, a ultricies nulla commodo non. Phasellus ac lacus ut diam finibus interdum."
+						text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet tortor eu justo sodales vehicula. Nulla auctor tempus arcu, a ultricies nulla commodo non. Phasellus ac lacus ut diam finibus interdum.",
+						number: 1,
+						correct: false,
+						rogues: [
+							{
+								name: "Bob Novella",
+								image: "test3.jpeg"
+							},
+							{
+								name: "Steve Novella",
+								image: "test4.jpeg"
+							},
+							{
+								name: "Jay Novella",
+								image: "test5.jpeg"
+							}
+						]
 					},
 					{
-						text: "Vestibulum vitae hendrerit massa. Nam pretium nibh ac ex tempor, eu posuere ex mattis. Cras libero diam, congue quis venenatis vel, gravida nec purus."
+						text: "Vestibulum vitae hendrerit massa. Nam pretium nibh ac ex tempor, eu posuere ex mattis. Cras libero diam, congue quis venenatis vel, gravida nec purus.",
+						number: 2,
+						correct: false
 					},
 					{
 						text: "Morbi eget tincidunt dolor. Nam egestas, leo id posuere laoreet, leo lectus auctor massa, vel convallis turpis enim at lectus.",
-						correct: true
+						number: 3,
+						correct: true,
+						rogues: [
+							{
+								name: "Evan Bernstein",
+								image: "test3.jpeg"
+							},
+							{
+								name: "Cara Santa Maria",
+								image: "test3.jpeg"
+							}
+						]
 					}
 				],
 				question: "Pick the fiction",
 				number: 1,
-				active: false,
-				answered: false
-			},
-			{
-				type: "normal",
-				answers: [
-					{
-						text: "Donec varius diam dignissim, placerat nisl at, imperdiet turpis. Cras eu pellentesque dolor, ut fermentum nunc."
-					},
-					{
-						text: "Nunc ex metus, lacinia facilisis lacinia nec, finibus id ante."
-					},
-					{
-						text: "Curabitur vitae velit quis dolor tempor lobortis in eu nunc. Nullam aliquam, libero rutrum convallis lacinia, elit tortor tincidunt dolor, venenatis cursus elit elit ut neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse potenti. ",
-						correct: true
-					}
-				],
-				question: "Pick the fiction",
-				number: 2,
 				active: true,
 				answered: false
 			},
@@ -207,14 +251,43 @@ function getFakeData() {
 				type: "normal",
 				answers: [
 					{
-						text: "Donec elementum nibh ac pharetra fermentum. Nunc sollicitudin, arcu nec accumsan pellentesque, tellus nisl pharetra nunc, id pharetra leo nulla nec lorem."
+						text: "Donec varius diam dignissim, placerat nisl at, imperdiet turpis. Cras eu pellentesque dolor, ut fermentum nunc.",
+						number: 1,
+						correct: false
 					},
 					{
-						text: "Donec elementum nibh ac pharetra fermentum. Nunc sollicitudin, arcu nec accumsan pellentesque, tellus nisl pharetra nunc, id pharetra leo nulla nec lorem."
+						text: "Nunc ex metus, lacinia facilisis lacinia nec, finibus id ante.",
+						number: 2,
+						correct: false
+					},
+					{
+						text: "Curabitur vitae velit quis dolor tempor lobortis in eu nunc. Nullam aliquam, libero rutrum convallis lacinia, elit tortor tincidunt dolor, venenatis cursus elit elit ut neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse potenti. ",
+						correct: true,
+						number: 3
+					}
+				],
+				question: "Pick the fiction",
+				number: 2,
+				active: false,
+				answered: false
+			},
+			{
+				type: "normal",
+				answers: [
+					{
+						text: "Donec elementum nibh ac pharetra fermentum. Nunc sollicitudin, arcu nec accumsan pellentesque, tellus nisl pharetra nunc, id pharetra leo nulla nec lorem.",
+						number: 1,
+						correct: false
+					},
+					{
+						text: "Donec elementum nibh ac pharetra fermentum. Nunc sollicitudin, arcu nec accumsan pellentesque, tellus nisl pharetra nunc, id pharetra leo nulla nec lorem.",
+						correct: true,
+						number: 2
 					},
 					{
 						text: "In varius tellus sed ullamcorper sagittis. Ut a eleifend diam. In eget commodo neque. Donec ac turpis sit amet sapien rhoncus cursus et finibus urna.",
-						correct: true
+						number: 3,
+						correct: false
 					}
 				],
 				question: "Pick the fiction",
