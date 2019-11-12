f64b8b51793b61b1e218f95da409dfdc5ed2a99e
Brad Thiessen <bthiessen@lawdepot.com>
2019-11-12 13:00:03 -0700

    ASDF

--- Calendar/js/site.js
+++ Calendar/js/site.js
@@ -172,7 +172,44 @@ function getQuizPercentage() {
 }
 
 
-
+const ProgressRing = Vue.component('progress-ring', {
+	props: {
+	  radius: String,
+	  progress: Number,
+	  stroke: String
+	},
+	data() {
+	  const normalizedRadius = this.radius - this.stroke * 2;
+	  const circumference = normalizedRadius * 2 * Math.PI;
+	  
+	  return {
+		normalizedRadius,
+		circumference
+	  };
+	},
+	computed: {
+	  strokeDashoffset() {
+		return this.circumference - this.progress / 100 * this.circumference;
+	  }
+	},
+	template: `
+	  <svg
+		:height="radius * 2"
+		:width="radius * 2"
+	   >
+		 <circle
+		   stroke="white"
+		   :stroke-dasharray="circumference + ' ' + circumference"
+		   :style="{ strokeDashoffset: strokeDashoffset }"
+		   :stroke-width="stroke"
+		   fill="transparent"
+		   :r="radius"
+		   :cx="radius"
+		   :cy="radius"
+		/>
+	  </svg>
+	`
+  });
 
 
 
